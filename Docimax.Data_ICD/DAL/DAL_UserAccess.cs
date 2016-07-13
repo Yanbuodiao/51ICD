f96f325386126e07610ebd80fab6ad13f4e7dc56
Yanbuodiao <yanbuodiao@hotmail.com>
2016-07-13 10:59:35 +0800

    少提交一个文件

---
+++ Docimax.Data_ICD/DAL/DAL_UserAccess.cs
@@ -0,0 +1,26 @@
+﻿using Docimax.Interface_ICD.Interface;
+using Docimax.Interface_ICD.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Docimax.Data_ICD.DAL
+{
+    public class DAL_UserAccess : IUserAccess
+    {
+        public bool ApplyIdentityVerify(VerifyIdentityModel Model)
+        {
+            using (var entity = new Entities_Write())
+            {
+                //using (TransactionScope transaction = new TransactionScope())
+                //{
+                //}
+                //Database.BeginTransaction();
+                return false;
+            }
+        }
+    }
+}
