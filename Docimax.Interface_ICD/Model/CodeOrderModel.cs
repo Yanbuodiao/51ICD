a2c2f0e450d546c58b7441696066d2392647428e
Yanbuodiao <yanbuodiao@hotmail.com>
2016-07-14 18:11:06 +0800

    编码订单实体修改

---
+++ Docimax.Interface_ICD/Model/CodeOrderModel.cs
@@ -0,0 +1,25 @@
+﻿using Docimax.Interface_ICD.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Docimax.Interface_ICD.Model
+{
+    public class CodeOrderModel
+    {
+        public int CodeOrderID { get; set; }
+        public string PlatformOrderCode { get; set; }
+        public string CaseNum { get; set; }
+        public ICDOrderState OrderStatus { get; set; }
+        public int ORGID { get; set; }
+        public int ORGSubID { get; set; }
+        public DateTime Createtime { get; set; }
+        public string CreateUserID { get; set; }
+        public DateTime LastModifyTime { get; set; }
+        public string LastModifyUserID { get; set; }
+        public int DeleteFlag { get; set; }
+        List<UploadItemModel> 
+    }
+}
