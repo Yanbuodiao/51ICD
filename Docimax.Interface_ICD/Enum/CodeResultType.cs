1c033bb2e00511c02fa4e8cd006d4bf413ee95d6
Yanbuodiao <yanbuodiao@hotmail.com>
2016-07-14 18:04:15 +0800

    状态整理

---
+++ Docimax.Interface_ICD/Enum/CodeResultType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Docimax.Interface_ICD.Enum
+{
+    public enum CodeResultType
+    {
+        主要诊断,
+        次要诊断,
+        主要操作,
+        次要操作,
+    }
+}
