c1aa19161c777ca5906e3c1dbccd949f2a034269
Yanbuodiao <yanbuodiao@hotmail.com>
2016-08-16 13:39:40 +0800

    开始服务申请

---
+++ Docimax.Interface_ICD/Enum/ServiceClaimType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Docimax.Interface_ICD.Enum
+{
+    public enum ServiceClaimType
+    {
+        编码诊断版本=10,
+        编码操作和手术版本=20
+    }
+}
